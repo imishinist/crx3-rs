ffba87ff4726c88784b4a31d41c792588bf5f1fb
Taisuke Miyazaki <imishinist@gmail.com>
2025-03-30 19:48:54 +0900

    build if proto file changed

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
     prost_build::compile_protos(&["src/proto/crx3.proto"], &["src/proto"])
         .expect("Failed to compile protos");
+    println!("cargo:rerun-if-changed=src/proto/crx3.proto");
 }
